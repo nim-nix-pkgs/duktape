21af51665dfbc6cb9d0d358fc3ec9add88fca62f
hugosenari <hugosenari@gmail.com>
2022-03-12 02:48:32 -0300

    chore: re index duktape

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''wrapper for the Duktape embeddable Javascript engine'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."duktape-master".url = "path:./master";
+  inputs."duktape-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."duktape-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
---
+++ master/flake.nix
@@ -0,0 +1,34 @@
+{
+  description = ''wrapper for the Duktape embeddable Javascript engine'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-duktape-master.flake = false;
+  inputs.src-duktape-master.owner = "manguluka";
+  inputs.src-duktape-master.ref   = "refs/heads/master";
+  inputs.src-duktape-master.repo  = "duktape-nim";
+  inputs.src-duktape-master.type  = "github";
+  
+  inputs."nimgen".dir   = "nimpkgs/n/nimgen";
+  inputs."nimgen".owner = "riinr";
+  inputs."nimgen".ref   = "flake-pinning";
+  inputs."nimgen".repo  = "flake-nimble";
+  inputs."nimgen".type  = "github";
+  inputs."nimgen".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nimgen".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-duktape-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-duktape-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
